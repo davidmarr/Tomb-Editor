6d78afdfe6afae94de05c71468e0c332623bcc3b
Kewin Kupilas <k.kupilas@outlook.com>
2026-03-31 21:42:09 +0100

    Revert "Fix FormFlybyCamera not restoring values when closed via X button"
    This reverts commit a89b371960d8deac7b09204dce387615981173c8.

--- TombEditor/Forms/FormFlybyCamera.cs
+++ TombEditor/Forms/FormFlybyCamera.cs
@@ -108,7 +108,7 @@ namespace TombEditor.Forms
 
         protected override void OnFormClosed(FormClosedEventArgs e)
         {
-            if (DialogResult != DialogResult.OK)
+            if (DialogResult == DialogResult.Cancel)
                 RestoreOriginalValues();
 
             // Only exit the preview mode if we were the one who entered it.
